11305b61490474bfe4865856a6b7c41f8589fc87
Billy Kwok <billykwok@Billys-MacBook-Pro.local>
2017-04-15 15:13:13 +0800

    make favicon

--- app.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable global-require, import/no-unresolved, no-console */
-try {
-  require('./dist/server.js');
-} catch (ex) {
-  console.error('Error', ex.message);
-  throw Error('./dist/server.js does not exist or contains error.');
-}
-/* eslint-enable global-require, import/no-unresolved, no-console */
